3ee01912e029225cff9260219ae1d27f66403cc0
Kirren Covey <kirrencovey@gmail.com>
2019-03-12 21:44:58 -0500

    removed unnecessary comments

--- src/components/behavior/BehaviorList(OneAnimal).js
+++ src/components/behavior/BehaviorList(OneAnimal).js
@@ -2,9 +2,6 @@ import React, { Component } from "react"
 
 export default class BehaviorList extends Component {
 
-
-    // ******************* WORKING ********************
-
     // Set initial state
     state = {
         behaviorId: ""
@@ -38,9 +35,6 @@ export default class BehaviorList extends Component {
                 behaviorId: ""
         })
         }
-
-        // **************************************************
-
 
     render() {
 
