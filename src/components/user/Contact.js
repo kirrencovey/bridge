94388c9201304cf38793361fdc954f422a13f0c7
Kirren Covey <kirrencovey@gmail.com>
2019-03-25 16:53:15 -0500

    fixed sticky footer

--- src/components/user/Contact.js
+++ src/components/user/Contact.js
@@ -10,14 +10,13 @@ class Contact extends Component {
                     <div className="homeCard">
                         <h1 className="homeTitle">bridge!</h1>
                         <br /><br />
-                        We're in beta testing. Your feedback is essential!
+                        During beta testing your feedback is essential!
                         <br /><br />
-                        Please report any bugs to:
+                        Please report any bugs to Kirren at:
                         <div className="emphasize">trainingbridge.app@gmail.com</div>
                         <br /><br /><br /><br /><br />
-                        If you find this app valuable, and would like to contribute financially to support development during the beta period, my venmo handle is @kirrencovey
-                        <div className="deemphasize">No amount is too small, and any help is immensely appreciated!</div>
-                        <br />
+                        <div className="deemphasize">If you would like to support development of this app, donations through venmo are greatly appreciated @kirrencovey</div>
+                        <br /><br />
                         <div className="emphasize">Thank you for using bridge!</div>
                     </div>
             </React.Fragment>
