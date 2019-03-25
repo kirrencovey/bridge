bdc8db5970c709f99005f21b430595cd1c3f0dc1
Kirren Covey <kirrencovey@gmail.com>
2019-03-25 13:32:01 -0500

    delete confirm modals for behaviors and animals

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react"
-import { Button } from 'reactstrap'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 export default class AnimalCard extends Component {
+
+    // Set initial state
+  constructor(props) {
+    super(props)
+    this.state = {
+      modal: false
+  }
+
+    this.toggle = this.toggle.bind(this)
+  }
+
+toggle() {
+  this.setState(prevState => ({
+    modal: !prevState.modal
+  }))
+}
+
     render() {
         return (
             <React.Fragment>
+
+                {/* confirm delete modal */}
+                <Modal isOpen={this.state.modal} toggle={this.toggle} >
+                    <ModalHeader toggle={this.toggle}>Wait!</ModalHeader>
+                    <ModalBody>
+                        Are you sure you want to delete this animal? Doing so will delete all associated training records.
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="info" onClick={() => {
+                            this.props.deleteAnimal(this.props.animal.id)
+                            this.toggle()
+                        }}>Delete</Button>
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+
                 <div className="cardContents">
                     <img className="cardImage" src={this.props.animal.image} />
                     <div className="cardTextContents">
@@ -22,10 +55,7 @@ export default class AnimalCard extends Component {
                                 className="btn-delete"
                                 type="button"
                                 onClick={() => {
-                                    let confirm = window.confirm("Are you sure you want to delete this animal? Doing so will delete all associated training records.")
-                                    if (confirm === true) {
-                                        this.props.deleteAnimal(this.props.animal.id)
-                                    }
+                                    this.toggle()
                                 }}
                             ><i className="fas fa-trash-alt"></i></Button>
                         </div>
