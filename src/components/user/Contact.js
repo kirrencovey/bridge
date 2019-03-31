9ade7bcce931bf25418ae7d9c99a68c65672d0a0
Kirren Covey <kirrencovey@gmail.com>
2019-03-31 14:30:09 -0500

    contact email is now a link that opens mail app

--- src/components/user/Contact.js
+++ src/components/user/Contact.js
@@ -12,10 +12,11 @@ class Contact extends Component {
                         <br /><br />
                         During beta testing your feedback is essential!
                         <br /><br />
-                        Please report any bugs to Kirren at:
-                        <div className="emphasize">trainingbridge.app@gmail.com</div>
-                        <br /><br /><br /><br /><br />
-                        <div className="deemphasize">If you would like to support the development of this app, donations through venmo are greatly appreciated <br /> @kirrencovey</div>
+                        Please report any bugs to Kirren at:<br />
+                        <a className="emphasize" href="mailto:trainingbridge.app@gmail.com">trainingbridge.app@gmail.com</a>
+                        <br /><br /><br />
+                        <div className="deemphasize">If you would like to support the development of this app, donations of any size are greatly appreciated! </div>
+                        <div>venmo: @kirrencovey</div>
                         <br /><br />
                         <div className="emphasize">Thank you for using bridge!</div>
                     </div>
